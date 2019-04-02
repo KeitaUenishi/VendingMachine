8ac1d4fc3237b95553e2c29e12850257a3ce092b
koujienami <test@example.com>
2019-04-02 17:08:11 +0900

    SQLの外部キーの設定誤りを修正。

--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -3,22 +3,24 @@ CREATE TABLE IF NOT EXISTS Product(
          ,name              varchar      (32)              NOT NULL
          ,amount            int          (5)    Default 0  NOT NULL
          ,salesAvailability varchar      (1)    Default 0  NOT NULL
-) ENGINE=INNODB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS Stock(
           id                varchar      (6)              NOT NULL  PRIMARY KEY
          ,productId         varchar      (6)              NOT NULL
          ,quantity          int          (2)   Default 0  NOT NULL
          ,replenishmentTime timestamp                     NOT NULL
-         FOREIGN KEY (productId)
-         REFERENCES Product (id)
-) ENGINE=INNODB DEFAULT CHARSET=utf8;
+         ,CONSTRAINT fk_stock_productId
+         	FOREIGN KEY (productId)
+         	REFERENCES Product (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS SalesHistory(
           id                varchar      (6)              NOT NULL  PRIMARY KEY
          ,productId         varchar      (6)              NOT NULL
          ,amount            int          (5)   Default 0  NOT NULL
          ,time              timestamp                     NOT NULL
-         FOREIGN KEY (productId)
-         REFERENCES Product(id)
-) ENGINE=INNODB DEFAULT CHARSET=utf8;
+         ,CONSTRAINT fk_salesHistory_productId
+         	FOREIGN KEY (productId)
+         	REFERENCES Product (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
