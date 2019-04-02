6ee84fc06c6ffa5fa61e2f17c10b05009c220152
koujienami <test@example.com>
2019-04-02 11:37:10 +0900

    SpringBootの雛形を作成。

---
+++ src/main/java/java/ddd/study/VendingMachine/ServletInitializer.java
@@ -0,0 +1,13 @@
+package java.ddd.study.VendingMachine;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(VendingMachineApplication.class);
+	}
+
+}
